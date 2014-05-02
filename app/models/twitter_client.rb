b0e3e23d0f680132bc75f9e82e66f1f9eab6c1c1
Garrett Martin <garrettqmartin8@optimum.net>
2014-05-02 18:00:19 -0500

    spacing

--- app/models/twitter_client.rb
+++ app/models/twitter_client.rb
@@ -1,8 +1,8 @@
 class TwitterClient
   def initialize
     Twitter.configure do |config|
-      config.consumer_key        = ENV["CONSUMER_KEY"]
-      config.consumer_secret     = ENV["CONSUMER_SECRET"]
+      config.consumer_key       = ENV["CONSUMER_KEY"]
+      config.consumer_secret    = ENV["CONSUMER_SECRET"]
       config.oauth_token        = ENV["ACCESS_TOKEN"]
       config.oauth_token_secret = ENV["ACCESS_TOKEN_SECRET"]
     end
