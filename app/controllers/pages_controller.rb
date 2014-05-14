3743f734c9418af1fa2befaeb6bafa38794f010e
tesstryan <tesstryan@gmail.com>
2014-05-13 22:29:54 -0500

    Added Current Conditions to Weather Model & View

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
     @last_song = "#{@rdio_client.song_name} by #{@rdio_client.artist_name}"
     @last_gram_image = InstagramClient.new.last_gram_image
     @current_temperature = Weather.new.get_current_temperature
+    @current_condition = Weather.new.get_current_condition
   end
 end
 
--- app/models/weather.rb
+++ app/models/weather.rb
@@ -3,12 +3,17 @@ class Weather
 require 'open-uri'
 require 'json'
 
-
-  def get_current_temperature
+  def get_data
     forecast_url = "http://api.openweathermap.org/data/2.5/weather?q=Chicago,IL"
     string_data = open(URI.encode(forecast_url)).read
     data = JSON.parse(string_data)
 
+    return data
+  end
+
+
+  def get_current_temperature
+    data = get_data
     temp = data["main"]["temp"]
     fahrenheit = (temp - 273.15)* 1.8000 + 32.00
     current_temp_degree = fahrenheit.to_i
@@ -17,5 +22,11 @@ require 'json'
 
   end
 
+  def get_current_condition
+    data = get_data
+    condition = data["weather"].first["description"]
+
+    return condition
+  end
 
 end
